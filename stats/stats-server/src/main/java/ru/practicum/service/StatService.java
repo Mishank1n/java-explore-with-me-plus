80f223f53131dc63b15aa0c5c73deaadba5b8960
Rifnur Vakhitov <rifnur69v@yandex.ru>
2025-09-24 01:20:17 +0300

    CheckStyle

--- stats/stats-server/src/main/java/ru/practicum/service/StatService.java
+++ stats/stats-server/src/main/java/ru/practicum/service/StatService.java
@@ -4,9 +4,7 @@ import ru.practicum.dto.StatisticsGetResponseDto;
 import ru.practicum.dto.StatisticsPostResponseDto;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 public interface StatService {
     StatisticsPostResponseDto hit(StatisticsPostResponseDto statisticsPostResponseDto);
