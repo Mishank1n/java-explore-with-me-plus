f58fb12ab7655d2fc6c0fb30a212aec72ae07089
Mihail_Kurenysev <michaelkurenyshev@yandex.ru>
2025-09-27 02:18:14 +0300

    checkstyle

--- main-service/src/main/java/ru/practicum/request/repository/RequestRepository.java
+++ main-service/src/main/java/ru/practicum/request/repository/RequestRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.request.model.Request;
-import ru.practicum.user.model.User;
 
 import java.util.List;
 
