e28c412f9d572aa0b15fe4099b6ce8c649c11860
Rifnur Vakhitov <rifnur69v@yandex.ru>
2025-09-27 04:08:54 +0300

    изменил ошибки

--- main-service/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -6,41 +6,24 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
-
 @RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler({PaginationException.class, NotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(final RuntimeException e) {
-        return new ErrorResponse(
-                "NOT_FOUND",
-                "The required object was not found.",
-                e.getMessage(),
-                LocalDateTime.now()
-        );
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(ValidationException e) {
-        return new ErrorResponse(
-                "BAD_REQUEST",
-                "Incorrectly made request.",
-                e.getMessage(),
-                LocalDateTime.now()
-        );
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler({BadParameterException.class, CreateConditionException.class, DataConflictException.class, ConflictException.class, ConstraintException.class})
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleConstraintException(final RuntimeException e) {
-        return new ErrorResponse(
-                "FORBIDDEN",
-                "For the requested operation the conditions are not met.",
-                e.getMessage(),
-                LocalDateTime.now()
-        );
+        return new ErrorResponse(e.getMessage());
     }
 }
--- main-service/src/main/java/ru/practicum/exception/ErrorResponse.java
+++ main-service/src/main/java/ru/practicum/exception/ErrorResponse.java
@@ -1,19 +1,13 @@
 package ru.practicum.exception;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class ErrorResponse {
-    private final String status;
-    private final String reason;
-    private final String message;
-    private final String timestamp;
-    private static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    private final String error;
 
-    public ErrorResponse(String status, String reason, String message, LocalDateTime timestamp) {
-        this.status = status;
-        this.reason = reason;
-        this.message = message;
-        this.timestamp = timestamp.format(DateTimeFormatter.ofPattern(DATE_TIME_PATTERN));
+    public ErrorResponse(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
     }
 }
