e9f20b5c841f063b063dc12452f2ed7e97be642f
Rifnur Vakhitov <rifnur69v@yandex.ru>
2025-09-30 00:51:13 +0300

    Добавил Post и Path методы

--- main-service/src/main/java/ru/practicum/comment/model/dto/CommentResponse.java
+++ main-service/src/main/java/ru/practicum/comment/model/dto/CommentResponse.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 public class CommentResponse {
     Long id;
     String text;
-    LocalDateTime created;
+    String created;
     Long author;
     Long event;
 }
--- main-service/src/main/java/ru/practicum/comment/service/CommentService.java
+++ main-service/src/main/java/ru/practicum/comment/service/CommentService.java
@@ -1,4 +1,10 @@
 package ru.practicum.comment.service;
 
+import ru.practicum.comment.model.dto.CommentRequest;
+import ru.practicum.comment.model.dto.CommentResponse;
+
 public interface CommentService {
+    CommentResponse createComment(Long userId, Long eventId, CommentRequest commentRequest);
+
+    CommentResponse updateComment(Long userId, Long eventId, CommentRequest commentRequest);
 }
