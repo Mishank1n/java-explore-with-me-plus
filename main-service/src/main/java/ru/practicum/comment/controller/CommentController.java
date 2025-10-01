3d5905416eaa4a99c4c6b494619fe588f3c3982a
Rifnur Vakhitov <rifnur69v@yandex.ru>
2025-10-01 16:04:02 +0300

    CheckStyle

--- main-service/src/main/java/ru/practicum/comment/controller/CommentController.java
+++ main-service/src/main/java/ru/practicum/comment/controller/CommentController.java
@@ -16,39 +16,44 @@ import java.util.List;
 public class CommentController {
 
     private final CommentService commentService;
+    private static final String EVENT_ID = "eventId";
+    private static final String COMMENT_ID = "commentId";
+    private static final String X_USER_ID = "X-User-Id";
+    private static final String COMMENT_ID_PATH = "/{commentId}";
+
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CommentResponse createComment(@PathVariable("eventId") Long eventId,
-                                         @RequestHeader("X-User-Id") Long userId,
+    public CommentResponse createComment(@PathVariable(EVENT_ID) Long eventId,
+                                         @RequestHeader(X_USER_ID) Long userId,
                                          @Valid @RequestBody CommentRequest commentRequest) {
         return commentService.createComment(userId, eventId, commentRequest);
     }
 
-    @PatchMapping("/{commentId}")
-    public CommentResponse updateComment(@PathVariable("eventId") Long eventId,
-                                         @PathVariable("commentId") Long commentId,
-                                         @RequestHeader("X-User-Id") Long userId,
+    @PatchMapping(COMMENT_ID_PATH)
+    public CommentResponse updateComment(@PathVariable(EVENT_ID) Long eventId,
+                                         @PathVariable(COMMENT_ID) Long commentId,
+                                         @RequestHeader(X_USER_ID) Long userId,
                                          @Valid @RequestBody CommentRequest commentRequest) {
         return commentService.updateComment(userId, commentId, commentRequest);
     }
 
     @GetMapping
-    public List<CommentResponse> getComments(@PathVariable("eventId") Long eventId) {
+    public List<CommentResponse> getComments(@PathVariable(EVENT_ID) Long eventId) {
         return commentService.getCommentsByEvent(eventId);
     }
 
-    @GetMapping("/{commentId}")
-    public CommentResponse getComment(@PathVariable("eventId") Long eventId,
-                                      @PathVariable("commentId") Long commentId) {
+    @GetMapping(COMMENT_ID_PATH)
+    public CommentResponse getComment(@PathVariable(EVENT_ID) Long eventId,
+                                      @PathVariable(COMMENT_ID) Long commentId) {
         return commentService.getCommentById(eventId, commentId);
     }
 
-    @DeleteMapping("/{commentId}")
+    @DeleteMapping(COMMENT_ID_PATH)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteComment(@PathVariable("eventId") Long eventId,
-                              @PathVariable("commentId") Long commentId,
-                              @RequestHeader("X-User-Id") Long userId) {
+    public void deleteComment(@PathVariable(EVENT_ID) Long eventId,
+                              @PathVariable(COMMENT_ID) Long commentId,
+                              @RequestHeader(X_USER_ID) Long userId) {
         commentService.deleteComment(userId, commentId);
     }
 
